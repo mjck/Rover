f5135d1b81c277b64cdc16ec3b59c5faccfe08f4
Marcel <marcel@nooroot.com>
2024-07-19 13:17:36 -0300

    Initial Commit

---
+++ Rover/RoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoverApp.swift
+//  Rover
+//
+//  Created by Marcel Jackowski on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct RoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
